1854e329f53e74d8e3a3d581eefaf23ff8e952dc
christian-hall <christian.hall395@gmail.com>
2021-04-13 20:10:44 -0400

    fixed dictionary formatting

--- palingram-finder.py
+++ palingram-finder.py
@@ -2,11 +2,10 @@
 import os
 
 dictionary = []
-try:
-    with open(os.path.dirname(os.path.abspath(__file__)) + '/dictionary.txt') as file:
-        for line in file:
-            dictionary.append(line.rstrip('\r\n'))
-        file.close()
-except IOErrror as e:
-    print('{}\nError opening {}. Terminating program.' .format(e, file), file.sys.stderr)
-    sys.exit(1)
+with open(os.path.dirname(os.path.abspath(__file__)) + '/dictionary.txt') as file:
+    for line in file:
+        dictionary.append(line.rstrip('\\\r\n'))
+    file.close()
+
+for entry in dictionary:
+    print(entry)
