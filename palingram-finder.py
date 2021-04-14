91f90c7e67311bd00ca5c1d8a8efe9165b638cf4
christian-hall <christian.hall395@gmail.com>
2021-04-13 20:32:53 -0400

    completed ability to find single palindromes

--- palingram-finder.py
+++ palingram-finder.py
@@ -1,11 +1,33 @@
 # dictionary imported from https://greenteapress.com/thinkpython2/code/words.txt\
 import os
 
-dictionary = []
+exit, dictionary = 'y', []
 with open(os.path.dirname(os.path.abspath(__file__)) + '/dictionary.txt') as file:
     for line in file:
         dictionary.append(line.rstrip('\\\r\n'))
     file.close()
 
-for entry in dictionary:
-    print(entry)
+while exit.lower() == 'y':
+    print('OPTIONS------------------------------')
+    print('1. List all palindromes in dictionary')
+    print('2. List all words that are palingrams')
+    print('3. List two words that are palingrams')
+    print()
+    choice = input('Enter choice: ')
+
+    if choice == '1':
+        for word in dictionary:
+            if word == word[::-1]:
+                print(word)
+
+    elif choice == '2':
+        print('2')
+        for word in dictionary:
+            idx = 1
+            while idx < len(word):
+                
+
+    else:
+        print('invalid choice')
+
+    exit = input('Go again? (y/n): ')
